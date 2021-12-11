fdf594f94e5156447cbf3a07c71a3c3824d6960a
Carl John Kugler III <carlk3@gmail.com>
2021-12-11 14:47:21 -0700

    Copyright notices

--- FreeRTOS+FAT+CLI/include/crash.h
+++ FreeRTOS+FAT+CLI/include/crash.h
@@ -1,3 +1,16 @@
+/* crash.h
+Copyright 2021 Carl John Kugler III
+
+Licensed under the Apache License, Version 2.0 (the License); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an AS IS BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+*/
 #ifndef _CRASH_H_
 #define _CRASH_H_
 // Original from M0AGX (blog@m0agx.eu), "Preserving debugging breadcrumbs across
